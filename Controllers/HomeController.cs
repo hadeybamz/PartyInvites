1a745fc629eb2b4a79c78587b0fc9887f13e830c
hadeybamz <adeyemi.bamgbala@gmail.com>
2021-03-26 18:26:54 +0100

    Party RSVP

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View();
+
+        [HttpGet]
+        public IActionResult RsvpForm() => View();
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse) {
+            if(ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            }
+            return View();
+        }
+
+        public ViewResult ListResponses() {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
+        }
+    }
+}
